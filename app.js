bcd3e196c9d21c8fbfaa7d01adb5bc568e5df0fd
Jair Partida <jairpartida1@gmail.com>
2021-05-12 22:58:54 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const countdown = () => {
+    const countDate = new Date("August 11, 2021 00:00:00").getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    document.querySelector(".day").innerHTML = textDay < 10 ? `0${textDay}` : textDay;
+    document.querySelector(".hour").innerHTML = textHour < 10 ? `0${textHour}` : textHour;
+    document.querySelector(".minute").innerHTML = textMinute < 10 ? `0${textMinute}` : textMinute;
+    document.querySelector(".second").innerHTML = textSecond < 10 ? `0${textSecond}` : textSecond;
+};
+
+setInterval(countdown, 1000);
