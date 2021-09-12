af1a806f4104ec83c098cd5c673eab10f86e2fe5
Jair Partida <jairpartida1@gmail.com>
2021-09-11 21:00:46 -0600

    updating the date

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("August 11, 2021 00:00:00").getTime();
+    const countDate = new Date("December 24, 2021 00:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
